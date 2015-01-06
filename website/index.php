7e174e22e49523c833a917f9ffda7cf8d83e22f3
Sebastian Leitz <sebasti@nleitz.de>
2015-01-06 16:20:22 +0100

    Common work progress

---
+++ website/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Semantic Search</title>
+    </head>
+    <body>
+        <form method="get">
+            <div class="formbody">
+                <input type="search">
+            </div>
+        </form>
+    </body>
+<?php
+    echo 'Hallo Welt';
+?>
+</html>
