d959182c72d9ea5d21783d90a263d756f1600252
Sebastian Leitz <sebasti@nleitz.de>
2015-01-16 21:34:29 +0100

    Created basic search interface for SPARQL queries

--- website/index.php
+++ website/index.php
@@ -1,16 +1,50 @@
+<?php
+    
+    if ($results = isset($_GET['query'])) {
+        require_once("sparqllib.php");
+        $db = sparql_connect("http://localhost:3030/ds/query");
+        $query = htmlspecialchars($_GET['query']);
+        $result = $db->query($query);
+    }
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <title>Semantic Search</title>
     </head>
     <body>
+        <h1>Movie Query Engine</h1>
         <form method="get">
             <div class="formbody">
-                <input type="search">
+                <input name="query" type="search" placeholder="Enter SPARQL here"<?php if ($results): ?> value="<?php echo $query; ?>"<?php endif; ?>>
+                <input type="submit" value="Query">
             </div>
         </form>
+<?php if (isset($result)): ?>
+<?php $fields = $result->field_array(); ?>
+        <div class="results">
+            <h2>Results (<?php echo $result->num_rows(); ?>)</h2>
+            <table>
+                <thead>
+                    <tr>
+<?php foreach ($fields as $field): ?>
+                        <th><?php echo $field; ?></th>
+<?php endforeach; ?>
+                    </tr>
+                </thead>
+                <tbody>
+<?php while ($row = $result->fetch_array()): ?>
+                    <tr>
+<?php foreach ($fields as $field): ?>
+                        <td><?php echo $row[$field]; ?></td>
+<?php endforeach; ?>
+                    </tr>
+<?php endwhile; ?>
+                </tbody>
+            </table>
+        </div>
+<?php endif; ?>
     </body>
-<?php
-    echo 'Hallo Welt';
-?>
 </html>
